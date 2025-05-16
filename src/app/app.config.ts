823922c2ddbef45c89a57f26847cd8b4d56be76d
bobyang08250772 <87290044+bobyang08250772@users.noreply.github.com>
2025-05-16 16:44:16 +0200

    database updated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXM26oXFsQPPI_L56xEMo_pwTkPRntdd4",
-  authDomain: "dbnotes-d5a86.firebaseapp.com",
-  databaseURL: "https://dbnotes-d5a86-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dbnotes-d5a86",
-  storageBucket: "dbnotes-d5a86.firebasestorage.app",
-  messagingSenderId: "480187997992",
-  appId: "1:480187997992:web:ff089f7b40ca76a4d57a1d",
+  projectId: " danotes-ee13b",
+
 };
 
 export const appConfig: ApplicationConfig = {
--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collectionData, collection, doc, onSnapshot, CollectionReference } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, doc,onSnapshot, CollectionReference, QuerySnapshot, DocumentData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Note } from '../interfaces/note.interface';
-import { DocumentData, QuerySnapshot } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +11,30 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
-  unsubList;
-  // unsubSingle;
-  // items$;
-
+  item$;
+  items;
+  // unsubList;
   firestore: Firestore = inject(Firestore);
 
   constructor() { 
-    this.unsubList = onSnapshot(
-      this.getNormalNoteRef(),
-      (snapshot: QuerySnapshot<DocumentData>) => {
-        this.normalNotes = snapshot.docs.map(doc => ({
-          title: doc.id,
-        })) as Note[];
+    // this.unsubList = onSnapshot(
+    //   this.getNormalNoteRef(),
+    //   (list) => {
+    //     list.forEach(e => {
+    //       console.log(e);
+          
+    //     })
+    //   }
+    // );
 
-        console.log(this.normalNotes);
-      }
-    );
-
+    this.item$ = collectionData(this.getNormalNoteRef());
+    console.log(this.item$);
+    this.items = this.item$.subscribe((list)=>{
+      list.forEach(e => {
+        console.log(e);
+      })
+    })
+    
   }
 
   getNormalNoteRef() {
